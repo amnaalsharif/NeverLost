cc3dfbb06afcf6962f11e1b2030c950d23bed8d8
Amna <aisharif@qatar.cmu.edu>
2016-10-17 18:10:50 +0300

    restrict dashboard to admin only

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,9 +7,11 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
+    before_action :authenticate_user!
+
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to '/', alert: 'Not authorized.' unless current_user && current_user.admin?
     end
 
     # Override this value to specify the number of elements to display at a time
